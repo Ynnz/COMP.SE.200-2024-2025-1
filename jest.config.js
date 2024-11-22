a1cd2f25786b34035749dee0d19c5cb986dde876
Yue <yue.zhu@tuni.fi>
2024-11-22 12:47:13 +0200

    add coverage configurations

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,17 @@ export default {
         "^.+\\.jsx?$": "babel-jest",
     },
     testEnvironment: "jest-environment-node",
+    "jest": {
+        "collectCoverage": true,
+        "coverageDirectory": "coverage",
+        "collectCoverageFrom": [
+            "src/ceil.js",
+            "src/divide.js",
+            "src/filter.js",
+            "src/isEmpty.js",
+            "src/map.js",
+            "src/toNumber.js"
+        ],
+        "testMatch": ["**/__tests__/jest/*.test.js"]
+    }
 };
