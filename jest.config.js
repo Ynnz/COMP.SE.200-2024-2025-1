69b694c6021c0106626a36d910295023ab340e67
Yue <yue.zhu@tuni.fi>
2024-11-22 17:33:41 +0200

    Revert "add coverage configurations"
    This reverts commit a1cd2f25786b34035749dee0d19c5cb986dde876.
    
    revert

--- jest.config.js
+++ jest.config.js
@@ -3,17 +3,4 @@ export default {
         "^.+\\.jsx?$": "babel-jest",
     },
     testEnvironment: "jest-environment-node",
-    "jest": {
-        "collectCoverage": true,
-        "coverageDirectory": "coverage",
-        "collectCoverageFrom": [
-            "src/ceil.js",
-            "src/divide.js",
-            "src/filter.js",
-            "src/isEmpty.js",
-            "src/map.js",
-            "src/toNumber.js"
-        ],
-        "testMatch": ["**/__tests__/jest/*.test.js"]
-    }
 };
