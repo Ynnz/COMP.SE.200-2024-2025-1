af90c786195df0055c7709d529f90ab20ee4f256
Yue <yue.zhu@tuni.fi>
2024-11-22 20:14:42 +0200

    add coverall configurations

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,15 @@ export default {
         "^.+\\.jsx?$": "babel-jest",
     },
     testEnvironment: "jest-environment-node",
+    collectCoverage: true,
+    coverageDirectory: "coverage/jest",
+    collectCoverageFrom: [
+        "src/ceil.js",
+        "src/divide.js",
+        "src/filter.js",
+        "src/isEmpty.js",
+        "src/map.js",
+        "src/toNumber.js",
+    ],
+    testMatch: ["**/__tests__/jest/*.test.js"],
 };
